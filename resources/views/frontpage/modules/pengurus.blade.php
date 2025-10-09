0ad4f91a82de4d10a25255f398699c6352fb84f6
farel_aja <116055126+farelaja-dev@users.noreply.github.com>
2025-10-09 16:43:24 +0700

    fix final

--- resources/views/frontpage/modules/pengurus.blade.php
+++ resources/views/frontpage/modules/pengurus.blade.php
@@ -17,11 +17,11 @@
         <section class="bg-[#FEF9F1] relative px-4 py-16 overflow-hidden md:px-6 md:py-24">
             <!-- Background Patterns -->
             <!-- <div class="absolute top-0 left-0 opacity-20">
-                                                                                                                                        <div class="w-64 h-64 bg-gradient-to-br from-gray-200 to-gray-300 rounded-full"></div>
-                                                                                                                                </div>
-                                                                                                                                    <div class="absolute bottom-0 right-0 opacity-20">
-                                                                                                                                        <div class="w-64 h-64 bg-gradient-to-br from-gray-200 to-gray-300 rounded-full"></div>
-                                                                                                                                    </div> -->
+                                                                                                                                            <div class="w-64 h-64 bg-gradient-to-br from-gray-200 to-gray-300 rounded-full"></div>
+                                                                                                                                    </div>
+                                                                                                                                        <div class="absolute bottom-0 right-0 opacity-20">
+                                                                                                                                            <div class="w-64 h-64 bg-gradient-to-br from-gray-200 to-gray-300 rounded-full"></div>
+                                                                                                                                        </div> -->
 
             <div class="container-responsive relative z-10 flex justify-center">
                 <div class="flex flex-col items-center">
@@ -113,6 +113,9 @@
                                     }
                                     if ($user->name === "Ulul 'Azmi") {
                                         return '3';
+                                    }
+                                    if ($user->name === 'Aulia Putri Rachmawati') {
+                                        return '4';
                                     }
                                     // Anggota lain diurutkan berdasarkan nama
                                     return '9' . $user->name;
@@ -283,6 +286,9 @@
                                             }
                                             if ($user->name === "Ulul 'Azmi") {
                                                 return '3';
+                                            }
+                                            if ($user->name === 'Aulia Putri Rachmawati') {
+                                                return '4';
                                             }
                                             // Anggota lain diurutkan berdasarkan nama
                                             return '9' . $user->name;
